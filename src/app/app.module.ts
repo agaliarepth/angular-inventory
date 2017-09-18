a558cce5de5fbd879ae8b68921a90541f0a91015
elver vasquez <vasquezelver@gmail.com>
2017-09-18 19:09:47 +0200

    respaldo del sistema de inventario

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, FormsModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { ProductsListComponent } from './products-list/products-list.component';
+import { ProductRowComponent } from './product-row/product-row.component';
+import { ProductImageComponent } from './product-image/product-image.component';
+import { ProductDisplayComponent } from './product-display/product-display.component';
+import { ProductDepartmentComponent } from './product-department/product-department.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductsListComponent,
+    ProductRowComponent,
+    ProductImageComponent,
+    ProductDisplayComponent,
+    ProductDepartmentComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
